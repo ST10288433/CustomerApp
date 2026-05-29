b1ce6fed21929a7a56bca56e363cae8848a18138
ST10288433 <126710590+ST10288433@users.noreply.github.com>
2026-05-29 20:32:25 +0200

    Update to files

--- obj/Debug/net9.0/CustomerApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/CustomerApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2b9cf8c1b9077d454b480aa872c01c10ed0e152")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+234ee9364db6ba61613ae9b7687dc08138021d7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
