7a40cfde1d863c31517ccdb22acb5efc2685d66f
ST10288433 <126710590+ST10288433@users.noreply.github.com>
2026-05-29 16:34:32 +0200

    Updated Program.cs and appsettings.json

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Database - connection string stored in appsettings.json for security
 builder.Services.AddDbContext<CustomerAppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CustomerApp_DB")));
 
@@ -14,7 +15,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Customer/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -28,7 +29,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Customer}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
